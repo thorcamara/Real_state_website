47d0de4cee177490cb514fa283eea4e1fa2de540
thorcamara <thorcamaraalthoff@gmail.com>
2023-11-06 00:26:53 -0300

    Added JS to Value Accordion

--- js/script.js
+++ js/script.js
@@ -16,3 +16,31 @@ var swiperPopular = new Swiper(".popular__container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+accordionItems.forEach((item) =>{
+  const accordionHeader = item.querySelector('.value__accordion-header')
+
+  accordionHeader.addEventListener('click', () =>{
+    const openItem = document.querySelector('accordion-open')
+
+    toggleItem(item)
+
+    if(openItem && openItem!== item){
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.value__accordion-content')
+
+  if(item.classList.contains('accordion-open')){
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  }else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
