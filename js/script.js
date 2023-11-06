d6887389ff13a6bcab5ec47b9facc9d0b7c5c000
thorcamara <thorcamaraalthoff@gmail.com>
2023-11-06 00:15:49 -0300

    Added JS to Swiper Popular

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ function scrollHeader(){
   if(this.scrollY >= 50) header.classList.add('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader)
+
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
