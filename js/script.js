5697224f93dc6dfa8c8f5592f710f374339c947d
thorcamara <thorcamaraalthoff@gmail.com>
2023-11-12 21:39:05 -0300

    Added JS to Scroll Sections Active Link

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function scrollHeader(){
+function scrollHeader() {
   const header = document.getElementById('header')
-  if(this.scrollY >= 50) header.classList.add('scroll-header');
+  if (this.scrollY >= 50) header.classList.add('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -19,28 +19,47 @@ var swiperPopular = new Swiper(".popular__container", {
 
 const accordionItems = document.querySelectorAll('.value__accordion-item')
 
-accordionItems.forEach((item) =>{
+accordionItems.forEach((item) => {
   const accordionHeader = item.querySelector('.value__accordion-header')
 
-  accordionHeader.addEventListener('click', () =>{
+  accordionHeader.addEventListener('click', () => {
     const openItem = document.querySelector('accordion-open')
 
     toggleItem(item)
 
-    if(openItem && openItem!== item){
+    if (openItem && openItem !== item) {
       toggleItem(openItem)
     }
   })
 })
 
-const toggleItem = (item) =>{
+const toggleItem = (item) => {
   const accordionContent = item.querySelector('.value__accordion-content')
 
-  if(item.classList.contains('accordion-open')){
+  if (item.classList.contains('accordion-open')) {
     accordionContent.removeAttribute('style')
     item.classList.remove('accordion-open')
-  }else{
+  } else {
     accordionContent.style.height = accordionContent.scrollHeight + 'px'
     item.classList.add('accordion-open')
   }
 }
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const scrollHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
